fe42964be50d1d6bfd95d190e2f2b99b2f75a8fe
Rune Warhuus <rune@dinkdonk.no>
2024-08-25 21:32:53 +0200

    Added macos install script

--- macos-install.sh
+++ macos-install.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-echo $1
+if [ $1 = "dark" ]; then
+	THEME="dark"
+else
+	THEME="light"
+fi
+
+KITTY_PATH=$(which kitty | sed "s/MacOS\/kitty//g")
+ICON_PATH=$KITTY_PATH"Resources/kitty.icns"
+
+curl -o $ICON_PATH "https://raw.githubusercontent.com/DinkDonk/kitty-icon/main/kitty-${THEME}.icns"
+rm /var/folders/*/*/*/com.apple.dock.iconcache; killall Dock
